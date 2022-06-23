f433a3797209afc72978614678f20cc591c18f5c
nazar-yednak <93836409+nazar-yednak@users.noreply.github.com>
2022-06-23 18:52:13 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,114 @@
+
+import NewsApiService from './js/apiService'
+import Notiflix from 'notiflix'
+
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const refs = {
+    searchForm: document.querySelector('#search-form'),
+    input: document.querySelector('input'),
+    galery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+    img: document.querySelector('img')
+
+
+}
+
+const newsApiServise = new NewsApiService()
+
+
+
+refs.galery.innerHTML = "";
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.galery.addEventListener('click', onImageClick);
+
+
+
+// let searchQ = "";
+function onSearch(e) {
+   
+   e.preventDefault()
+   
+  const form = e.currentTarget;
+  if (e.currentTarget.elements.searchQuery.value === ""   ) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+  }
+   
+  else {  
+    newsApiServise.query = e.currentTarget.elements.searchQuery.value;
+    newsApiServise.resetPage()
+    resetSearchGallery()
+    newsApiServise.fetchCard().then(renderGaleryCard).catch(error => { console.log(error); }
+   
+    );
+
+    refs.loadMoreBtn.classList.remove('is-hidden')
+  
+    form.reset();
+    
+  }
+}
+
+
+
+function renderGaleryCard(data) {   
+ 
+const markup =  data.hits.map(({ largeImageURL, tags, likes, views, comments, downloads }) => {
+    return `<div class="photo-card">
+<a href="${largeImageURL }"><img src="${largeImageURL}" alt="${tags}" loading="lazy" /></a> 
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+ </div>
+</div>`
+   })
+  refs.galery.insertAdjacentHTML('beforeend', markup)
+  Notiflix.Notify.success(`Sol lucet "Hooray! We found ${data.totalHits} images.`);
+
+}
+function onLoadMore(hits) {
+
+  newsApiServise.fetchCard().then(data => {
+    if (data.hits.length === 0) {
+
+      refs.loadMoreBtn.classList.add('is-hidden');
+
+      Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`);
+      return;
+    }
+    else {
+      newsApiServise.fetchCard().then(renderGaleryCard)
+
+       
+    }
+  }).catch(error => { console.log(error); })
+}
+ 
+
+function resetSearchGallery(){
+  refs.galery.innerHTML= ""
+}
+
+function onImageClick(e) {
+  e.preventDefault();  
+  let galleryCard = new SimpleLightbox('a', { captionDelay: 250 });
+  galleryCard.refresh();
+  refs.galery.addEventListener("click", galleryCard);
+}
+
+
+
---
+++ src/js/apiService.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+
+
+
+const BASE_URL = 'https://pixabay.com/api';
+
+
+export default class newsApiServise {
+    constructor() {
+        this.searchQ = '';
+        this.page = 1;
+        
+    }
+    fetchCard() {
+        const options = {
+          key: '28055079-45e3c423b4c5366c3627ebf35',
+            q: '',
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: "true",
+            page: this.page,
+            per_page:40
+     }
+    const url = `${BASE_URL}/?key=${options.key}&q=${this.searchQ}&image_type=${options.photo}&orientation=${options.orientation}&safesearch=${options.safesearch}&&page=${this.page}&per_page=${options.per_page}`
+        
+        return axios.get(url, options)
+            .then(response => {            
+                this.incrementPage() 
+                console.log(response);
+             return response.data
+              
+            })    
+  
+    }
+
+    incrementPage() {
+        this.page += 1
+      
+    }
+    resetPage() {
+        this.page = 1;
+    }
+    get query () {
+        return this.searchQ
+    }
+    set query(newQuery)
+    {
+        this.searchQ = newQuery
+    }
+}
